de738bb2e9fa2e989446ee452306b7bdde675df6
Adam Measel <ameasel55@gmail.com>
2023-01-27 11:56:52 -0600

    boomhauer

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Man I tell you what Hank bout there-that dang ol meaning o’life, man. It’s like this man.");
+Console.WriteLine("You’ like a butterfly flappin ‘is wings deep down in that forest man an’ it gonna cause a tree fall like five thousand miles away man.");
+Console.WriteLine("If-an ain’t no body see it nobody don-done-e’en know it happen you know ibda baby born into this world int’know neck god dang friends got no nothin but da go come into find out about em ol evil man.");
+Console.WriteLine("Man see like, you don even know man. When-dyagon it’d like you born into this world man and you got — it’s like this:");
+Console.WriteLine("dust in the wind man, or like a dang ol’ candle in the wind man.");
+Console.WriteLine("You gon — it don matter man it’s not the old oldies all th’ time man. You know what I think man? It’d like the the dang ol – I think therefore you are man.");
